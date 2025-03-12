2d4f6ddff2fbe7074db549071db7d5e2cbc0dde8
liliyafedyurina <liliya.fedyurina@gmail.com>
2025-03-12 11:18:19 -0700

    added .end, gitignore, and updated application schema

--- models/user.js
+++ models/user.js
@@ -1,6 +1,30 @@
 const mongoose = require('mongoose');
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 
+            'applied', 
+            'interviewing', 
+            'rejected', 
+            'accepted'],
+  },
+});
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -9,6 +33,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema], // embedding the applicationSchema here
 });
 
 const User = mongoose.model('User', userSchema);
