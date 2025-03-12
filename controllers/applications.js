ed0ac1fabb91d7bac953ca66f230dd7e2e459f54
liliyafedyurina <liliya.fedyurina@gmail.com>
2025-03-12 13:44:26 -0700

    wip:created application.js

--- controllers/applications.js
+++ controllers/applications.js
@@ -6,5 +6,20 @@ const router = express.Router();
 const User = require('../models/user.js');
 
 // we will build out our router logic here
+//GET /users/:userId/applications
 
+router.get('/', (req, res)=>{
+res.send('Hello applications index route!')
+})
+
+router.get('/', async (req, res) =>{
+    try {
+        res.render('applications/index.ejs')
+        
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+      }
+    });
+    
 module.exports = router;
